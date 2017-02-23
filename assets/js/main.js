4ec864c52bce1fbfcc338281e11b793b7d316be0
Maria <mariacutipa.gonzales@gmail.com>
2017-02-23 16:50:42 -0500

    Listar un array

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+var arreglo= [  { nombre : "Arabela", apellido : "Rojas", rol : "T.A", cumpleanios : "23 de enero" },
+                { nombre : "Michelle", apellido : "Seguil", rol : "T.A", cumpleanios : "25 de noviembre" },
+                { nombre : "Meche", apellido : "Zubieta", rol : "T.A", cumpleanios : "02 de marzo" },
+                { nombre : "Papu", apellido : "Rivariola", rol : "Psicóloga", cumpleanios : "10 de octubre" },
+                { nombre : "Gian", apellido : "Corzo", rol : "Profesor", cumpleanios : "23 de enero" } ]
+
+for(var i=0 ; i< arreglo.length ;i++){
+  document.getElementById("mostrar").innerHTML +=
+          "<ul>" +
+          "<li>Nombre: " + arreglo[i].nombre + "</li>" +
+          "<li>Apellido: " + arreglo[i].apellido  + "</li>" +
+          "<li>DNI: " + arreglo[i].rol  + "</li>" +
+          "<li>Dirección: " + arreglo[i].cumpleanios  + "</li>" +
+        "</ul>";
+
+}
+
